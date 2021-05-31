c8c6899e6e14cce406513b3db2be5cc94c7c0759
--no-verify <amnaazhar94@gmail.com>
2021-05-30 23:32:38 -0400

    update

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ const quote = [
 var quote_index = 0;
 const camera = new THREE.PerspectiveCamera( 100, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
